898840c6dc0a44c08a2e730e7742841dc15968f6
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2019-06-28 10:33:03 +0200

    ...

--- docker/centos7-runner/kaniko-build.sh
+++ docker/centos7-runner/kaniko-build.sh
@@ -7,4 +7,4 @@ KANIKO_OPTS=${KANIKO_OPTS:-"--insecure --skip-tls-verify"}
 KANIKO_EXECUTOR=${KANIKO_EXECUTOR:-"/kaniko/executor"}
 
 ${KANIKO_EXECUTOR} -f "$(pwd)/Dockerfile" -c "$(pwd)" ${KANIKO_OPTS} \
-  --destination ${DOCKER_REGISTRY_HOST}/${IMAGE}:${TAG}
+  --destination https://${DOCKER_REGISTRY_HOST}/${IMAGE}:${TAG}
